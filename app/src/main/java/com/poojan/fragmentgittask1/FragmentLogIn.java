343c44bf1b67f41db792262466b54203bdedbf17
PoojanParikh <camkampar@gmail.com>
2017-02-03 12:39:44 +0530

    Fragment Login Completed

---
+++ app/src/main/java/com/poojan/fragmentgittask1/FragmentLogIn.java
@@ -0,0 +1,24 @@
+package com.poojan.fragmentgittask1;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by VNurtureTechnologies on 03/02/17.
+ */
+
+public class FragmentLogIn extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.login_fragment,container,false);
+
+        return view;
+    }
+}
